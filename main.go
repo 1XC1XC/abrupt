df1dfb6090634161fa23e4c871450a3e53d797a7
1XC <62459731+1XCRLKQtMTGAQAvSxv0Yb@users.noreply.github.com>
2021-11-18 01:34:36 -0600

    package

--- main.go
+++ main.go
@@ -1,10 +1,8 @@
-package main
+package abrupt
 
-// import (
-// 	"github.com/1XC1XC/abrupt.go/"
-// 	"fmt"
-// )
-
-// func main() {
-// 	// fmt.Println(Crypto.Hex.Encode("asdfasdfasdf"))
-// }
+import (
+	"github.com/1XC1XC/abrupt.go/Crypto"
+	"github.com/1XC1XC/abrupt.go/File"
+	"github.com/1XC1XC/abrupt.go/Rand"
+	"github.com/1XC1XC/abrupt.go/String"
+)
