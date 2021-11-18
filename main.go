097413451730b57d795fbd84298c127d6ced2852
1XC <62459731+1XCRLKQtMTGAQAvSxv0Yb@users.noreply.github.com>
2021-11-18 02:30:16 -0600

    Update main.go

--- main.go
+++ main.go
@@ -1,12 +1,8 @@
 package abrupt
 
 import (
-	C "github.com/1XC1XC/abrupt/Crypto"
-	F "github.com/1XC1XC/abrupt/File"
-	R "github.com/1XC1XC/abrupt/Rand"
-	S "github.com/1XC1XC/abrupt/String"
-)
-
-const (
-	Crypto, File, Rand, String = C, F, R, S
+	_ "github.com/1XC1XC/abrupt/Crypto"
+	_ "github.com/1XC1XC/abrupt/File"
+	_ "github.com/1XC1XC/abrupt/Rand"
+	_ "github.com/1XC1XC/abrupt/String"
 )
